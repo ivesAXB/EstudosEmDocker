9a2e42fcddb55059971689d5a89ad4b6020b5aa9
Ives de Arruda Xavier Botelho <ives.botelho@liberali.local>
2025-10-28 15:02:13 -0400

    Commit inicial

---
+++ backend/src/main/java/br/com/meusprojetos/awards_api/AwardsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.meusprojetos.awards_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwardsApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/meusprojetos/awards_api/AwardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meusprojetos.awards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
